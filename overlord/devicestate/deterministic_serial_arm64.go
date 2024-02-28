67ba7b73c29c8b84b32d88db9631fe8c38cda26b
Florian Metzger-Noel <florian.metzger-noel@ml-pa.com>
2024-02-28 08:15:02 +0100

    fix: missing import

--- overlord/devicestate/deterministic_serial_arm64.go
+++ overlord/devicestate/deterministic_serial_arm64.go
@@ -2,6 +2,8 @@
 
 package devicestate
 
+import "github.com/snapcore/snapd/asserts"
+
 // getDeviceSerial generates a deterministic serial number for the device from the TPM's Endorsement Key.
 func getDeviceSerial() (string, error) {
 	return asserts.TpmDeterministicDeviceSerial()
