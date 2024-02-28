82f3a13d034f599d15ce94d0ae64360c7ae997b5
Florian Metzger-Noel <florian.metzger-noel@ml-pa.com>
2024-02-28 08:15:18 +0100

    feat: improved output for tpm CLI tool

--- cmd/m2cp-tpm/main.go
+++ cmd/m2cp-tpm/main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"github.com/snapcore/snapd/asserts"
-	"os"
 )
 
 func main() {
+	fmt.Print("Generating deterministic serial: ")
 	serial, err := asserts.TpmDeterministicDeviceSerial()
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		os.Exit(1)
+		fmt.Printf("failed: %s\n", err)
+	} else {
+		fmt.Printf("success\n%s\n", serial)
 	}
+	fmt.Println()
+
+	fmt.Print("Getting m2cp-public-key: ")
 	key, err := asserts.TpmGetEndorsementPublicKey()
 	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		os.Exit(1)
+		fmt.Printf("failed: %s\n", err)
+	} else {
+		fmt.Printf("success\npub key sha3-384: %s\n", key.ID())
 	}
-	fmt.Printf("m2cp signing key sha3-384..: %s\n", key.ID())
-	fmt.Printf("deterministic device serial: %s\n", serial)
+	fmt.Println()
+
+	fmt.Print("Signing 'hello-world'..\n")
+	signature, err := asserts.TpmSignBytes([]byte("hello world"))
+	if err != nil {
+		fmt.Printf("failed: %s\n", err)
+	} else {
+		fmt.Printf("\nsuccess\n%s\n", base64.StdEncoding.EncodeToString(signature))
+	}
+	fmt.Println()
 }
