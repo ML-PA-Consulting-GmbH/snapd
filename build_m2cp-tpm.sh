9893a9343d5a07a198d110acce30c68b48d98a78
Florian Metzger-Noel <florian.metzger-noel@ml-pa.com>
2024-03-01 08:45:33 +0100

    chore: build script update

--- build_m2cp-tpm.sh
+++ build_m2cp-tpm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BINARY_PATH="bin/m2cp-tpm/linux-arm64/m2cp-tpm"
+BINARY_PATH="bin/m2cp-tpm"
 env GOOS=linux GOARCH=arm64 CGO_ENABLED=1 CC="aarch64-linux-gnu-gcc" m2cp-go build -o "$BINARY_PATH" cmd/m2cp-tpm/**
 
 echo compiled to $BINARY_PATH
