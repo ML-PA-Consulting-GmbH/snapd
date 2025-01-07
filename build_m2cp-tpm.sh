8046ecbc352195e5193264668dd6099b78c62093
florian.metzger-noel <florian.metzger-noel@ml-pa.com>
2025-01-07 17:01:40 +0100

    feat: add TPM module

---
+++ build_m2cp-tpm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+BINARY_PATH="bin/m2cp-tpm"
+env GOOS=linux GOARCH=arm64 CGO_ENABLED=1 CC="aarch64-linux-gnu-gcc" m2cp-go build -o "$BINARY_PATH" cmd/m2cp-tpm/**
+
+echo compiled to $BINARY_PATH
