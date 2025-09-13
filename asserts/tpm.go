f0cb1c6af17a88e8c0968107e65e7d6e419e57b3
János Brodbeck <janos.brodbeck@ml-pa.com>
2025-09-14 00:05:26 +0200

    chore: remove extensive TPM logging

--- asserts/tpm.go
+++ asserts/tpm.go
@@ -44,30 +44,15 @@ func TpmSignBytes(toSign []byte) (signature []byte, err error) {
 	var sig []byte
 
 	if err = withTpm(func(key *client.Key) error {
-		fmt.Printf("signing %d bytes with key %s, payload (base64 encoded):\n%s\n",
-			len(toSign),
-			RSAPublicKey(key.PublicKey().(*rsa.PublicKey)).ID(),
-			base64.StdEncoding.EncodeToString(toSign),
-		)
-
 		digest := tpmHashBytes(toSign)
-		fmt.Printf("Step 1: hashed the payload with SHA3_384. digest base64 encoded:\n%s\n", base64.StdEncoding.EncodeToString(digest))
-
-		fmt.Printf("Step 2: sign the digest (hash again with sha2_256 and encrypt with RSA key)\n")
 		sig, err = key.SignData(digest)
 		if err != nil {
 			return fmt.Errorf("failed to sign: %s", err)
 		}
 
-		fmt.Printf("Step 3: created signature, base64 encoded:\n%s\n", base64.StdEncoding.EncodeToString(sig))
-
 		if !tpmVerifyEkSignature(key.PublicKey(), toSign, sig) {
 			return fmt.Errorf("signature verification failed")
 		}
-
-		id := RSAPublicKey(key.PublicKey().(*rsa.PublicKey)).ID()
-		fmt.Printf("signed message with key %s\n", id)
-
 		return nil
 	}); err != nil {
 		return nil, err
@@ -191,7 +176,6 @@ func withTpmTry(f func(key *client.Key) error) (err error) {
 	tpmLock.Lock()
 	defer tpmLock.Unlock()
 
-	log.Printf("opening TPM\n")
 	rwc, err := tpm2.OpenTPM("/dev/tpmrm0")
 	if err != nil {
 		return fmt.Errorf("failed to open TPM: %s", err)
@@ -200,20 +184,15 @@ func withTpmTry(f func(key *client.Key) error) (err error) {
 		_ = rwc.Close()
 	}()
 
-	log.Printf("fetching/creating key")
 	key, err := client.NewCachedKey(rwc, tpm2.HandleEndorsement, tpmSigningKeyTemplate, tpmSigningKeyHandle)
 	if err != nil {
 		return fmt.Errorf("failed to create key: %s", err)
 	}
 	defer key.Close()
 
-	log.Printf("ready for tpm operation")
-
 	err = f(key)
 	if err != nil {
 		log.Printf("tpm operation finished with error: %s", err)
-	} else {
-		log.Printf("tpm operation finished successfully")
 	}
 
 	return err
